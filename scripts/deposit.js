cf8dcf31f7a52e2f13b280ef91b8b94dae432318
Kirill Fedoseev <k.fedoseev@innopolis.university>
2021-10-26 19:19:57 +0300

    Add sanity checks

--- scripts/deposit.js
+++ scripts/deposit.js
@@ -2,10 +2,10 @@ const Web3 = require('web3')
 
 const { abi } = require('../build/contracts/IERC677.json')
 
-const { RPC_URL, DEPLOYMENT_ACCOUNT_PRIVATE_KEY, BATCH_SIZE, N, OFFSET, TOKEN_ADDRESS, DEPOSIT_CONTRACT_ADDRESS } = process.env
+const { RPC_URL, GAS_PRICE, STAKING_ACCOUNT_PRIVATE_KEY, BATCH_SIZE, N, OFFSET, TOKEN_ADDRESS, DEPOSIT_CONTRACT_ADDRESS } = process.env
 
 const web3 = new Web3(RPC_URL)
-const { address } = web3.eth.accounts.wallet.add(DEPLOYMENT_ACCOUNT_PRIVATE_KEY)
+const { address } = web3.eth.accounts.wallet.add(STAKING_ACCOUNT_PRIVATE_KEY)
 
 const depositData = require(process.argv[2])
 
@@ -27,7 +27,16 @@ async function main() {
     return
   }
 
-  console.log(`Sending ${Math.ceil(deposits.length / batchSize)} deposit transactions for ${deposits.length} deposits in batches of ${batchSize} events`)
+  const depositAmountBN =  web3.utils.toBN(32).mul(web3.utils.toBN('1000000000000000000'))
+  const tokenBalance = await token.methods.balanceOf(address).call()
+
+  if (web3.utils.toBN(tokenBalance).lt(depositAmountBN)) {
+    console.log(`Token balance is not enough to cover all deposits, have ${tokenBalance}, required ${depositAmountBN.toString()}`)
+    return
+  }
+
+  console.log(`Sending ${Math.ceil(deposits.length / batchSize)} deposit transactions for ${deposits.length} deposits in batches of ${batchSize} deposits`)
+  let balance = await web3.eth.getBalance(address).then(web3.utils.toBN)
   let nonce = await web3.eth.getTransactionCount(address)
   let count = 0
   let data = '0x' + wc
@@ -39,14 +48,28 @@ async function main() {
     count++
 
     if (count === batchSize || i === deposits.length - 1) {
-      const amount = web3.utils.toBN(32 * count).mul(web3.utils.toBN('1000000000000000000'))
+      const amount = depositAmountBN.muln(count)
       const call = token.methods.transferAndCall(DEPOSIT_CONTRACT_ADDRESS, amount, data)
-      const gas = await call.estimateGas({ from: address })
+      let gas
+      try {
+        gas = await call.estimateGas({ from: address })
+      } catch (e) {
+        console.log('Gas estimation failed:', e.message)
+        return
+      }
+      const gasLimit = Math.ceil(gas * 1.5)
+      const feeBN = web3.utils.toBN(GAS_PRICE).muln(gasLimit)
+      if (balance.lt(feeBN)) {
+        console.log(`Native balance is not enough to cover tx fee, have ${balance.toString()}, required ${feeBN.toString()}`)
+        return
+      }
       const receipt = await call.send({
         from: address,
         nonce: nonce++,
-        gas: Math.ceil(gas * 1.5),
+        gas: gasLimit,
+        gasPrice: GAS_PRICE,
       })
+      balance = balance.sub(web3.utils.toBN(GAS_PRICE).muln(receipt.gasUsed))
       console.log(`\t${count} next deposits: ${receipt.transactionHash}`)
       data = '0x' + wc
       count = 0
